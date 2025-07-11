6e48745f3ac1b73c8c340554193d7e3cdf4e0a7e
Muhammad Abdullah <abdullahmuh2004@gmail.com>
2025-07-11 11:08:27 +0500

    .NET 8 project added

---
+++ Task01/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task01.Model;
+
+namespace UserApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> users = new()
+        {
+            new User { Id = 1, Name = "Alice", Email = "alice@example.com" },
+            new User { Id = 2, Name = "Bob", Email = "bob@example.com" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAll()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetById(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            return user == null ? NotFound("User not found.") : Ok(user);
+        }
+
+        [HttpGet("find")]
+        public ActionResult<User> FindByEmail([FromQuery] string email)
+        {
+            var user = users.FirstOrDefault(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+            return user == null ? NotFound("User not found.") : Ok(user);
+        }
+    }
+}
---
+++ Task01/Model/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Task01.Model;
+
+public class User
+{
+    public required int  Id { get; set; }
+    public required String  Name { get; set; }
+    public required String Email { get; set; }
+}
